18e5018e114b5586681fdf529d954546fa1a197b
Joe Egboka <joeegboka@gmail.com>
2023-10-03 02:38:33 +0100

    add Technical interview preparation

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if list contains a cycle
+ * @list: pointer to list
+ *
+ * Return:  0 if there is no cycle, 1 if there is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *fast = list;
+
+	if (!fast)
+		return (0);
+	if (fast->next != NULL && fast->next->next != NULL)
+	{
+		list = list->next;
+		fast = fast->next->next;
+		if (fast == list)
+			return (1);
+	}
+
+	return (0);
+}
