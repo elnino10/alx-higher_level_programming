17b13fcb41161ac15b201aa59836d0df6fc70265
Joe Egboka <joeegboka@gmail.com>
2023-10-06 00:37:26 +0100

    100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -7,22 +7,21 @@ if __name__ == "__main__":
     if arg_size != 4:
         print("Usage: {} <a> <operator> <b>".format(argv[0]))
         exit(1)
-    else:
-        a = int(argv[1])
-        b = int(argv[3])
-        operator = argv[2]
+    a = int(argv[1])
+    b = int(argv[3])
+    operator = argv[2]
 
-        match operator:
-            case "+":
-                res = add(a, b)
-            case "-":
-                res = sub(a, b)
-            case "*":
-                res = mul(a, b)
-            case "/":
-                res = div(a, b)
-            case _:
-                print("Unknown operator. Available operators: +, -, * and /")
-                exit(1)
+    match operator:
+        case "+":
+            res = add(a, b)
+        case "-":
+            res = sub(a, b)
+        case "*":
+            res = mul(a, b)
+        case "/":
+            res = div(a, b)
+        case _:
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
 
-        print("{} {} {} = {}".format(a, operator, b, res))
+    print("{} {} {} = {}".format(a, operator, b, res))
