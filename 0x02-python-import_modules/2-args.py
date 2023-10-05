763b0ebb6920894e0f2856e939defd807d936021
Joe Egboka <joeegboka@gmail.com>
2023-10-05 21:01:36 +0100

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-from sys import argv
+if __name__ == "__main__":
+    from sys import argv
 
-argv_size = len(argv)
+    argv_size = len(argv)
 
-if argv_size < 2:
-    print("0 arguments.")
-elif argv_size == 2:
-    print("{} argument:".format(argv_size - 1))
-    print("{}: {}".format(argv_size - 1, argv[1]))
-else:
-    print("{} arguments:".format(argv_size - 1))
-    i = 0
-    while (i < argv_size - 1):
-        print("{}: {}".format(i + 1, argv[i + 1]))
-        i += 1
+    if argv_size < 2:
+        print("0 arguments.")
+    elif argv_size == 2:
+        print("{} argument:".format(argv_size - 1))
+        print("{}: {}".format(argv_size - 1, argv[1]))
+    else:
+        print("{} arguments:".format(argv_size - 1))
+        i = 0
+        while (i < argv_size - 1):
+            print("{}: {}".format(i + 1, argv[i + 1]))
+            i += 1
