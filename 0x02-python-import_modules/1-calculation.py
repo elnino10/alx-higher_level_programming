9cd76aca0d40e9608554ff8461e6251fd7c1ef66
Joe Egboka <joeegboka@gmail.com>
2023-10-05 18:58:25 +0100

    1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    import calculator_1 as calc
+    from calculator_1 import add, sub, mul, div
 
     a = 10
     b = 5
 
-    add = calc.add(a, b)
-    sub = calc.sub(a, b)
-    mul = calc.mul(a, b)
-    div = calc.div(a, b)
+    add = add(a, b)
+    sub = sub(a, b)
+    mul = mul(a, b)
+    div = div(a, b)
 
     print(f"{a:d} + {b:d} = {add:d}")
     print(f"{a:d} - {b:d} = {sub:d}")
