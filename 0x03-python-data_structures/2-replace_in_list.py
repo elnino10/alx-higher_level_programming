6afa3e07749339e5661e604eeb550a840db0f511
Joe Egboka <joeegboka@gmail.com>
2023-10-08 14:04:47 +0100

    2-replace_in_list.py

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def replace_in_list(my_list, idx, element):
+    list_len = len(my_list)
+
+    if idx < 0:
+        return my_list
+
+    if idx >= list_len:
+        return list_len
+
+    my_list[idx] = element
+    return my_list
