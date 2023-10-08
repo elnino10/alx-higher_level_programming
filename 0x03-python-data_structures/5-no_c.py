e6ba67e3c7f8d03e39012adc373a21a0c47222cc
Joe Egboka <joeegboka@gmail.com>
2023-10-08 15:34:10 +0100

    5-no_c.py

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+    new_string = ""
+
+    for el in my_string:
+        if el == 'c' or el == 'C':
+            continue
+        new_string += el
+
+    return new_string
