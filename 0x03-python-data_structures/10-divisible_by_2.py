b206746c97ddca8f727bbd92b6d876ef501e74c6
Joe Egboka <joeegboka@gmail.com>
2023-10-08 22:38:31 +0100

    10-divisible_by_2.py

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def divisible_by_2(my_list=[]):
+    res = []
+
+    for el in my_list:
+        if el % 2 == 0:
+            res.append(True)
+        else:
+            res.append(False)
+    return res
