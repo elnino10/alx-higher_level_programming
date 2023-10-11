b075a76a54d72816ac38fe88d082c574fe8378c5
Joe Egboka <joeegboka@gmail.com>
2023-10-11 20:22:11 +0100

    1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-    for ind in range(len(my_list)):
-        if my_list[ind] == search:
-            my_list[ind] = replace
-    return my_list
+    new_list = my_list[:]
+    for ind in range(len(new_list)):
+        if new_list[ind] == search:
+            new_list[ind] = replace
+    return new_list
