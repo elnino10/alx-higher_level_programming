ddd2e00a1f781cbd55bf2ba1696097dc33370f11
Joe Egboka <joeegboka@gmail.com>
2023-10-13 21:14:20 +0100

    12-roman_to_int.py

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+	roman_num = {
+			'I': 1,
+			'V': 5,
+			'X': 10,
+			'L': 50,
+			'C': 100,
+			'D': 500,
+			'M': 1000}
+	result = 0
+
+	prev_val = 0
+	for num in reversed(roman_string):
+		val = roman_num.get(num, 0)
+		if val >= prev_val:
+			result += val
+		else:
+			result -= val
+		prev_val = val
+
+	return result
