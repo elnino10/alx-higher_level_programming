b40ab86530e5cb01009da63ac67eb2fdf7073697
Joe Egboka <joeegboka@gmail.com>
2023-10-23 15:50:12 +0100

    add 0-safe_print_list.py and README.md files

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    list_len = 0
+
+    for el in range(x):
+        try:
+            print("{}".format(my_list[el]), end="")
+            list_len += 1
+        except IndexError:
+            break
+    print()
+    return list_len
