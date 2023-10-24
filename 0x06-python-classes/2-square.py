6d0d8313a95cbd17dc6ec2919419139a6bd99b18
Joe Egboka <joeegboka@gmail.com>
2023-10-25 00:13:25 +0100

    2-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Create a class that defines a Square"""
+
+
+class Square:
+    """A class that defines a square"""
+    __size = None
+
+    def __init__(self, size=0):
+        """Initialize class"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
