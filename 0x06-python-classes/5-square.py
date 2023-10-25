9806418ea38a93e8de9612075660be250e26a73e
Joe Egboka <joeegboka@gmail.com>
2023-10-25 01:56:37 +0100

    5-square.py

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Create a class that defines a Square"""
+
+
+class Square:
+    """A class that defines a square"""
+    __size = None
+
+    def __init__(self, size=0):
+        """Initialize class"""
+        self.__size = size
+
+    @property
+    def size(self):
+        """get the size"""
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """sets the size of the square instance"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Returns the area of square"""
+        return (self.__size ** 2)
+
+    def my_print(self):
+        """prints the square in character #"""
+        if self.__size == 0:
+            print("")
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
