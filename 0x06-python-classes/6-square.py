d1b6f9656be2a91974a292f64dcaf22d52a98e7a
Joe Egboka <joeegboka@gmail.com>
2023-10-25 02:20:23 +0100

    6-square.py

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+"""Create a class that defines a Square"""
+
+
+class Square:
+    """A class that defines a square"""
+    __size = None
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Initialize class"""
+        self.__size = size
+
+        if isinstance(position, tuple) and len(position) == 2 and\
+            isinstance(position[0], int) and isinstance(position[1], int) and\
+                position[0] >= 0 and position[1] >= 0:
+            self.__position = position
+        else:
+            raise TypeError("position must be a tuple of 2 positive integers")
+
+    @property
+    def size(self):
+        """get the size"""
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """sets the size of the square instance"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Returns the area of square"""
+        return (self.__size ** 2)
+
+    def my_print(self):
+        """prints the square in character #"""
+        if self.__size == 0:
+            print("")
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
+
+    @property
+    def position(self):
+        """Gets the position of the square.
+
+        Returns:
+            Position of the Square.
+        """
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """Sets the position of the square
+
+        Args:
+            value: value to set the position
+        """
+        if isinstance(value, tuple) and len(value) == 2 and\
+            isinstance(value[0], int) and isinstance(value[1], int) and\
+                value[0] >= 0 and value[1] >= 0:
+            self.__position = value
+        else:
+            raise TypeError("position must be a tuple of 2 positive integers")
