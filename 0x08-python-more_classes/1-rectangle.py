742a36d9566c7d70e792b8002057e0eeeb9d9a68
Joe Egboka <joeegboka@gmail.com>
2023-10-30 16:31:07 +0100

    1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -4,6 +4,9 @@
 
 class Rectangle():
     """Rectangle module that defines a rectangle"""
+    __width = None
+    __height = None
+
     def __init__(self, width=0, height=0):
         self.__width = width
         self.__height = height
