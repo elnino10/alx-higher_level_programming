35bca9241450b36f5b5f9d612f5204cbdfc5f49f
Joe Egboka <joeegboka@gmail.com>
2023-10-30 23:32:11 +0100

    101-nqueens.py

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """a program that solves the N queens problem
-N queens puzzle is the challenge of placing N 
+N queens puzzle is the challenge of placing N
 non-attacking queens on an NxN chessboard
 """
 
