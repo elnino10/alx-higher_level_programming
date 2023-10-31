3fda22f8474808a940b3a918649df40975e7da73
Joe Egboka <joeegboka@gmail.com>
2023-10-31 23:38:50 +0100

    0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """add_integer function returns the sum of two integers
+
 works for two integers
 >>> add_integer(2, 2)
 4
@@ -9,6 +10,9 @@ works for two integers
 def add_integer(a, b=98):
     """returns sum of two intergers
     numbers must be casted to integers before adding
+
+    >>> add_integer(-1, -2)
+    -3
     """
     if isinstance(a, float):
         a = int(a)
