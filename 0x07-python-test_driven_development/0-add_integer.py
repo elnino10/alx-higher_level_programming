a9c4851e5c13c668a6c3e5c60ca58b8cfe7eae66
Joe Egboka <joeegboka@gmail.com>
2023-11-01 14:30:29 +0100

    0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -14,15 +14,22 @@ def add_integer(a, b=98):
     >>> add_integer(-1, -2)
     -3
     """
+    max_float_num = 3.402823466e+38
+    min_float_num = 1.175494351e-38
+
+    if (a > max_float_num) or (a < min_float_num):
+        raise TypeError("a must be an integer")
+    if (b > max_float_num) or (b < min_float_num):
+        raise TypeError("b must be an integer")
+
     if not isinstance(a, (int, float)):
         raise TypeError('a must be an integer')
     if not isinstance(b, (int, float)):
         raise TypeError('b must be an integer')
-    
+
     if isinstance(a, float):
         a = int(a)
     if isinstance(b, float):
         b = int(a)
-
 
     return a + b
