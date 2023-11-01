b92249a8be5d5ecda9cfce52057cf624aa7d51a7
Joe Egboka <joeegboka@gmail.com>
2023-11-01 16:50:48 +0100

    tests/6-max_integer_test.py

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -11,10 +11,18 @@ class TestMaxInteger(unittest.TestCase):
     """Test for max_integer in list of integers"""
     def setUp(self):
         self.result1 = max_integer([])
-        self.result2 = max_integer([1, 2, 3, 5, 7, 4])
+        self.result2 = max_integer([1, 2, -3, 5, 7, 4])
         self.result3 = max_integer([4])
+        self.result4 = max_integer([1, 2, 3, 5, 4, 7])
+        self.result5 = max_integer([7, 1, 2, 3, 5, 4])
+        self.result6 = max_integer([7, 7, 7])
+        self.result7 = max_integer([-7, -5, -2, -3])
 
     def test_max_integer(self):
         self.assertEqual(self.result1, None, 'list is empty')
         self.assertEqual(self.result2, 7, 'incorrect result')
         self.assertEqual(self.result3, 4, 'incorrect result')
+        self.assertEqual(self.result4, 7, 'incorrect result')
+        self.assertEqual(self.result5, 7, 'incorrect result')
+        self.assertEqual(self.result6, 7, 'incorrect result')
+        self.assertEqual(self.result7, -2, 'incorrect result')
