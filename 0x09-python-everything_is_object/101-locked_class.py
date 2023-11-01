11251ba65f5d9c32254643a64032999130e28b75
Joe Egboka <joeegboka@gmail.com>
2023-11-01 23:31:54 +0100

    101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -8,11 +8,4 @@ called first_name
 class LockedClass():
     """prevents dynamic instance creation"""
 
-    def __setattr__(self, name, value):
-        """sets attribute passed by user"""
-        if name == "first_name":
-            super().__setattr__(name, value)
-        else:
-            raise AttributeError(
-                f"'{self.__class__.__name__}' object has no \
-attribute '{name}'")
+    __slots__ = ["first_name"]
