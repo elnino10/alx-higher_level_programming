8e5bcdcfd72c7518bc511fe267ef4688444727c7
Joe Egboka <joeegboka@gmail.com>
2023-11-01 14:16:02 +0100

    0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -14,9 +14,9 @@ def add_integer(a, b=98):
     >>> add_integer(-1, -2)
     -3
     """
-    if not isinstance(a, int):
+    if not isinstance(a, (int, float)):
         raise TypeError('a must be an integer')
-    if not isinstance(b, int):
+    if not isinstance(b, (int, float)):
         raise TypeError('b must be an integer')
     
     if isinstance(a, float):
