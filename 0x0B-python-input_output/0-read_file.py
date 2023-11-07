e17300103a70163536b09a816cd2ba6d0906d98d
Joe Egboka <joeegboka@gmail.com>
2023-11-07 19:33:02 +0100

    0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+0-read_file module has a function that reads a text file
+(UTF8) and prints it to stdout
+"""
+
+
+def read_file(filename=""):
+    """reads a text file and print to stdout
+
+    Args:
+        filename (str, optional): name of file to read from. Defaults to "".
+    """
+    with open(filename, encoding="utf-8") as my_file:
+        for line in my_file:
+            print(line, end="")
+        print()
