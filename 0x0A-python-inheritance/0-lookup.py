49b6694b6fa0c0ae5bb81440bcd3fd880f0f4a6c
Joe Egboka <joeegboka@gmail.com>
2023-11-07 01:05:49 +0100

    9-rectangle.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """0-lookup module with the lookup() function"""
 
+
 def lookup(obj):
 	"""returns the list of available attributes and methods of an object"""
 	return dir(obj)
