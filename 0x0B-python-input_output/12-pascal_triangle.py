a341ade571d480eb0e359cb8bee6bd2f207f5cec
Joe Egboka <joeegboka@gmail.com>
2023-11-08 16:48:27 +0100

    12-pascal_triangle.py

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""function def pascal_triangle(n): that returns a list of lists
+of integers representing the Pascal's triangle of n
+"""
+
+
+def pascal_triangle(n):
+    """returns a list of lists of integers representing the pascal's triangle
+
+    Args:
+        n (int): number of lists
+
+    Return:
+        an empty list if n <= 0, or a list of lists of integers otherwise
+    """
+    p_triangle = [[]]
+    row = 1
+    while row <= n:
+        triangle_row = []
+        for col in range(row):
+            # get value for current cell
+            if col in (0, row - 1):
+                # if first or last cell in current row
+                cell_val = 1
+            else:
+                # add the two cells above
+                cell_val = p_triangle[row - 1][col - 1] + p_triangle[row - 1][col]
+            triangle_row.append(cell_val)
+        p_triangle.append(triangle_row)
+        row += 1
+    # slice out the initializing empty list before returning
+    return p_triangle[1:]
