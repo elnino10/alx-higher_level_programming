f9af21b56517d3fd4be3ac7033d723d64f824833
Joe Egboka <joeegboka@gmail.com>
2023-11-11 23:06:08 +0100

    Update the class Square by adding the public method update()

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -26,3 +26,28 @@ class Square(Rectangle):
         return "[{}] ({}) {}/{} - {}".format(
             self.__class__.__name__, self.id, self.x, self.y, self.width
         )
+
+    @property
+    def size(self):
+        """returns size of the square sides"""
+        return self.width
+
+    @size.setter
+    def size(self, size):
+        """sets the size of the square side
+
+        Args:
+            size (int): size
+        """
+        self.width = size
+        self.height = size
+
+    def update(self, *args, **kwargs):
+        """assigns attributes"""
+        arg_list = ["id", "size", "x", "y"]
+        if args is not None and len(args) > 0:
+            for name, value in zip(arg_list, args):
+                setattr(self, name, value)
+        else:
+            for name, value in kwargs.items():
+                setattr(self, name, value)
