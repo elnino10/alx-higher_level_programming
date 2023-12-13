787eb9992620aa33c05c13472e913d05736f534c
Joe Egboka <joeegboka@gmail.com>
2023-12-13 02:27:24 +0100

    102-top_city.sql

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,5 @@
+-- Find hottest 3 cities for AUGUST and JULY
+-- Find the average for both  months
+SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE month = 7 OR month = 8 
+GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
+
