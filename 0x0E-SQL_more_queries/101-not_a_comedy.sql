9f9fdb4abad9fa9dada4684a0132b534e8648efc
Joe Egboka <joeegboka@gmail.com>
2023-12-15 00:48:28 +0100

    101-not_a_comedy.sql

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -2,8 +2,6 @@
 -- select shows without comedy genre
 SELECT DISTINCT tv_shows.title
 FROM tv_shows
-/*LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id*/
 WHERE tv_shows.title NOT IN (
 	SELECT tv_shows.title
 	FROM tv_shows
