438557af303602c3dafcdf1bfeead9ed8b9a8d1c
Joe Egboka <joeegboka@gmail.com>
2023-12-14 12:03:38 +0100

    16-shows_by_genre.sql

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,7 +1,8 @@
 -- lists all shows, and all genres linked to that show
 -- use SELECT statement
 SELECT tv_shows.title, tv_genres.name
-FROM tv_genres RIGHT OUTER JOIN tv_show_genres
+FROM tv_genres 
+RIGHT OUTER JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
 RIGHT OUTER JOIN tv_shows
 ON tv_shows.id = tv_show_genres.show_id
