0a40ed454e58b786e4ebe25292f2467b36dbee25
Joe Egboka <joeegboka@gmail.com>
2023-12-14 02:44:29 +0100

    16-shows_by_genre.sql

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -5,5 +5,5 @@ FROM tv_genres RIGHT OUTER JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
 RIGHT OUTER JOIN tv_shows
 ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.title;
+ORDER BY tv_shows.title, tv_genres.name;
 
