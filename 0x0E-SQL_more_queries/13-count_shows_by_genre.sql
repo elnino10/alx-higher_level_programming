971b376f2048599f52979189559b7523b24673b9
Joe Egboka <joeegboka@gmail.com>
2023-12-14 01:26:33 +0100

    13-count_shows_by_genre.sql

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,8 @@
+-- lists all genres from hbtn_0d_tvshows
+-- also displays the number of shows linked to each
+SELECT DISTINCT tv_genres.name AS name, COUNT(tv_show_genres.genre_id) AS number_of_shows
+FROM tv_genres JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_show_genres.genre_id
+ORDER BY number_of_shows DESC
+
