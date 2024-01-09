fd26d631c1c92ba08457b125ebe795c0ff3f1069
Joe Egboka <joeegboka@gmail.com>
2024-01-09 13:03:50 +0100

    11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,17 +4,21 @@ if (process.argv.length < 4) {
   console.log(0);
 } else {
   let firstMax = parseInt(process.argv[2]);
-  let secondMax = firstMax;
+  let secondMax = parseInt(process.argv[2]);
   for (let i = 2; i < process.argv.length; i++) {
     if (firstMax > parseInt(process.argv[i])) {
       if (secondMax < parseInt(process.argv[i])) {
         secondMax = parseInt(process.argv[i]);
       } else {
-        continue;
+        if (secondMax === firstMax) {
+          secondMax = parseInt(process.argv[i]);
+	} else {
+          continue;
+	}
       }
     } else {
-      secondMax = firstMax;
-      firstMax = parseInt(process.argv[i]);
+        secondMax = firstMax;
+        firstMax = parseInt(process.argv[i]);
     }
   }
   console.log(secondMax);
