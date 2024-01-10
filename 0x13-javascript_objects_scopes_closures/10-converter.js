00edb8074b3e1be83e443d1f1997839fcd6acd06
Joe Egboka <joeegboka@gmail.com>
2024-01-10 02:01:25 +0100

    10-converter.js

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,16 +1,7 @@
 #!/usr/bin/node
 
 exports.converter = function (base) {
-  return (num) => {
-    let result = '';
-    while (num > 0) {
-      const remainder = num % base;
-      result =
-        (remainder < 10
-          ? remainder
-          : String.fromCharCode(97 + remainder - 10)) + result;
-      num = Math.floor(num / base);
-    }
-    return result;
+  return function (num) {
+    return num.toString(base);
   };
 };
