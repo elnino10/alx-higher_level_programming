e177fbcb7e20740cf6ecd9095c2fe37feaadcd40
Joe Egboka <joeegboka@gmail.com>
2024-01-13 12:47:51 +0100

    0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,19 +6,23 @@ import sys
 import MySQLdb
 
 
-def list_states(username, password, database_name):
+def list_states(u_name, password, db_name):
     """creates a connection to database and executes query"""
     # connect to MySQL server
     db = MySQLdb.connect(
-        host="localhost", port=3306, user=username, passwd=password, db=database_name
-    )
+        host="localhost", port=3306, user=u_name, passwd=password, db=db_name)
 
-    # create cursor object to interact with the database
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
+    all_states = cur.fetchall()
+
+    for state in all_states:
+        print(state)
+    cur.close()
+    db.close()
 
 
 if __name__ == "__main__":
     # get information from the CLI arguments
-    usr_name, psswd, db_name = sys.argv[1:4]
-    list_states(usr_name, psswd, db_name)
+    usr_name, psswd, database_name = sys.argv[1:4]
+    list_states(usr_name, psswd, database_name)
