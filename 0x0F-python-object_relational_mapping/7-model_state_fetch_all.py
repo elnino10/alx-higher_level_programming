c6d827d665dc4536804d1035a0cda200924ae7d1
Joe Egboka <joeegboka@gmail.com>
2024-01-13 22:14:09 +0100

    7-model_state_fetch_all.py

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -10,8 +10,7 @@ from sqlalchemy.orm import sessionmaker
 
 from model_state import Base, State
 
-DATABASE_URI = f'mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}'
-DATABASE_URI += f'@localhost:3306/{sys.argv[3]}'
+DATABASE_URI = f'mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}@localhost:3306/{sys.argv[3]}'
 
 if __name__ == '__main__':
     engine = create_engine(DATABASE_URI, pool_pre_ping=True)
