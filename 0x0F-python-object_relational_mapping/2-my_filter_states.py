0eedf84472afae798bea9f3ab764dfd08f02a9bd
Joe Egboka <joeegboka@gmail.com>
2024-01-13 14:25:06 +0100

    2-my_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+module with a script that takes in an argument and displays all values in the
+states table of hbtn_0e_0_usa where name matches the argument
+"""
+import sys
+
+import MySQLdb
+
+
+def display_value(usr, passwd, db_name, state_name):
+    """displays values where state name matches state_name
+
+    Args:
+        usr (string): mysql username
+        passwd (string): mysql password
+        db_name (string): database name
+        state_name (string): state to search for
+    """
+
+    db_conn = MySQLdb.connect(
+        host="localhost", port=3306, user=usr, passwd=passwd, db=db_name
+    )
+    cursor = db_conn.cursor()
+    query = "SELECT * FROM states WHERE name=%s ORDER BY id ASC"
+    cursor.execute(query, (state_name,))
+    data = cursor.fetchall()
+
+    for val in data:
+        print(val)
+
+    cursor.close()
+    db_conn.close()
+
+
+if __name__ == "__main__":
+    u_name, password, database, state = sys.argv[1:5]
+    display_value(u_name, password, database, state)
