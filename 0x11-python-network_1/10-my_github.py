840eede13318e10612607d4cdb5eadb7283fff81
Joe Egboka <joeegboka@gmail.com>
2024-01-27 03:19:35 +0100

    10-my_github.py

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+script that takes your GitHub credentials (username and password)
+and uses the GitHub API to display your id
+"""
+if __name__ == "__main__":
+    import sys
+
+    import requests
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    payload = {"login": username}
+    url = f"https://api.github.com/users/"
+
+    res = requests.get(url, params=payload, auth=(username, password))
+    json_data = res.json()
+
+    if json_data:
+        print(json_data["id"])
+    else:
+        print("None")
