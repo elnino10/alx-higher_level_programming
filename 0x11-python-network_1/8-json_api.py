1f07d8622044d77ee0312629993124f722f1ee47
Joe Egboka <joeegboka@gmail.com>
2024-01-27 02:14:48 +0100

    8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -15,9 +15,9 @@ if __name__ == "__main__":
         else:
             var = {"q": sys.argv[1]}
         res = requests.post(url, var)
-        if res.status_code == 204:
+        if res.json().status_code == 204:
             print("No result")
         else:
-            print(res.json())
+            print(f"[{res.json().id}] {res.json().name}")
     except requests.exceptions.JSONDecodeError as e:
         print("Not a valid JSON")
