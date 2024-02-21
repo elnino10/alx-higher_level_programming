734c6d5900116909bfbf35b82469e1dbd2c76884
Joe Egboka <joeegboka@gmail.com>
2024-02-22 00:10:14 +0100

    6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,23 +1,30 @@
 #!/usr/bin/node
-// web scrapper
+// script that computes the number of tasks completed by user id
 
 const request = require('request');
 
-request.get(process.argv[2], { json: true }, (error, response, body) => {
-  if (error) {
-    console.log(error);
-    return;
-  }
+const apiUrl = process.argv[2];
 
-  const tasksCompleted = {};
-  body.forEach((todo) => {
-    if (todo.completed) {
-      if (!tasksCompleted[todo.userId]) {
-        tasksCompleted[todo.userId] = 1;
-      } else {
-        tasksCompleted[todo.userId] += 1;
-      }
+request.get(apiUrl, (error, response, body) => {
+  if (error) console.log(error);
+  const jsonData = JSON.parse(body);
+
+  let prevId = null;
+  let currId = null;
+  let count = 0;
+  const taskObj = {};
+
+  jsonData.forEach(el => {
+    if (el.userId !== prevId && prevId !== null) {
+      taskObj[`${prevId}`] = count;
     }
+    if (el.completed === true) {
+      currId = el.userId;
+      if (currId === prevId) count++;
+      else count = 1;
+    }
+    prevId = currId;
   });
-  console.log(tasksCompleted);
+  taskObj[`${currId}`] = count;
+  console.log(taskObj);
 });
