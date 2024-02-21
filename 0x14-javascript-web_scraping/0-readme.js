8c3600b1935a51dfcf81b064f8036e630e7b8c6f
Joe Egboka <joeegboka@gmail.com>
2024-02-21 13:13:24 +0100

    0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,22 @@
 #!/usr/bin/node
 // Reading from file
 
-const filesys = require('fs');
-filesys.readFile(process.argv[2], 'utf-8',
-  function (err, data) {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    console.log(data);
-  });
+// const filesys = require('fs');
+// filesys.readFile(process.argv[2], 'utf-8',
+//   function (err, data) {
+//     if (err) {
+//       console.log(err);
+//       return;
+//     }
+//     console.log(data);
+//   });
+
+const fs = require('fs')
+
+const filePath = process.argv[2]
+try {
+  const data = fs.readFileSync(filePath, 'utf-8');
+  console.log(data);
+} catch (err) {
+  console.log(err);
+}
