e95487acadaa9bd64b04c19515c34e39289221c7
Joe Egboka <joeegboka@gmail.com>
2024-02-21 20:37:00 +0100

    2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -5,5 +5,5 @@ const request = require('request');
 const url = process.argv[2];
 request.get(url, (error, response) => {
   if (error) console.log(error);
-  else console.log('code:', + ' ' + response && response.statusCode);
+  else console.log('code:', +' ' + response && response.statusCode);
 });
