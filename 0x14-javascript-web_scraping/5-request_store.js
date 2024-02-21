5abdc026279fae49ede754ef978e165d1d1e61bf
Joe Egboka <joeegboka@gmail.com>
2024-02-21 20:59:59 +0100

    5-request_store.js

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,17 +1,14 @@
 #!/usr/bin/node
-// lorem ipsum
-
+// script gets the contents of a webpage and stores it in a file
 const request = require('request');
 const fs = require('fs');
 
-request.get(process.argv[2], (error, response, body) => {
-  if (error) {
-    console.log(error);
-  } else {
-    fs.writeFile(process.argv[3], body, 'utf-8', (error) => {
-      if (error) {
-        console.log(error);
-      }
-    });
-  }
+const reqUrl = process.argv[2];
+const filePath = process.argv[3];
+
+request.get(reqUrl, (error, response, body) => {
+  if (error) console.log(error);
+  fs.writeFile(filePath, body, 'utf-8', (error) => {
+    if (error) console.log(error);
+  });
 });
