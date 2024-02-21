979b1951f4194af60d026bdca801470553a0296d
Joe Egboka <joeegboka@gmail.com>
2024-02-21 19:11:20 +0100

    3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
 // Star Wars
-
 const request = require('request');
 
 const movieId = process.argv[2];
-const urlEndpoint = 'https://swapi-api.alx-tools.com/api/films/'+ movieId;
+const urlEndpoint = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
 
 request.get(urlEndpoint, (error, response, body) => {
   if (error) console.log(error);
   const json_data = JSON.parse(body);
-  console.log(json_data['title']);
+  console.log(json_data.title);
 });
