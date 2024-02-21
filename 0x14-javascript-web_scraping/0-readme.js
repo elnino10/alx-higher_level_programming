ac4e1c1814fe1ab4c6511ba68263e98139e00673
Joe Egboka <joeegboka@gmail.com>
2024-02-21 13:14:33 +0100

    0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -11,9 +11,9 @@
 //     console.log(data);
 //   });
 
-const fs = require('fs')
+const fs = require('fs');
 
-const filePath = process.argv[2]
+const filePath = process.argv[2];
 try {
   const data = fs.readFileSync(filePath, 'utf-8');
   console.log(data);
