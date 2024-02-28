4df5a7e7db0d0fb020a0c9b11ba26a7b4471bda8
Joe Egboka <joeegboka@gmail.com>
2024-02-29 00:47:38 +0100

    100-script.js

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,8 @@
+// script that updates the text color of the <header> element to red (#FF0000)
+
+document.addEventListener('DOMContentLoaded', function () {
+  const header = document.querySelector('header');
+  if (header) {
+    header.style.color = '#FF0000';
+  }
+});
