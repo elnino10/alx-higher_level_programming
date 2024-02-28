29094cb322f1d10e338b1ef81178b22446f7aed5
Joe Egboka <joeegboka@gmail.com>
2024-02-29 00:59:19 +0100

    9-script.js

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -2,7 +2,7 @@
 //  URL: https://swapi-api.alx-tools.com/api/people/5/?format=json
 // The name must be displayed in the HTML tag DIV#character
 
-const url = 'https://swapi-api.alx-tools.com/api/people/5/?format=json'
+const url = 'https://swapi-api.alx-tools.com/api/people/5/?format=json';
 $.get(url, function (data) {
   $('DIV#character').text(data.name);
 });
